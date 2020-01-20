7f8e0a1d724ae09d40669bef60ba43bb74dbffe3
Niek van Gogh <ik@niekvangogh.nl>
2020-01-20 14:35:20 +0100

    omg fuck

--- pages/game/play/_gameId.vue
+++ pages/game/play/_gameId.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="d-flex justify-content-center mt-5">
-    <sudoku
-      v-if="grid"
-      :grid="grid"
-      @updateGuess="updateGuess"
-      @updatePotentialGuess="updatePotentialGuess"
-    />
+  <div>
+    <b-row>
+      <b-col cols="9">
+        <h2>You are currently playing against 3 opponents</h2>
+        <div class="d-flex justify-content-center mt-5">
+          <sudoku
+            v-if="grid"
+            :grid="grid"
+            @updateGuess="updateGuess"
+            @addPotentialTile="addPotentialTile"
+            @removePotentialTile="removePotentialTile"
+          />
+        </div>
+      </b-col>
+      <b-col cols="3">
+        <h5>Other players in the game:</h5>
+        <b-list-group>
+          <b-list-group-item
+            v-for="player in players"
+            :key="player.id"
+            :to="{ name: 'player-id', params: {id: player.id }}"
+          >{{player.username}} - {{player.ranking}}</b-list-group-item>
+        </b-list-group>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -19,20 +37,22 @@ export default {
   data() {
     return {
       gameId: this.$route.params.gameId,
-      grid: null
+      grid: null,
+      players: []
     };
   },
   mounted() {
-    // if (
-    //   this.$socketManager.stompClient &&
-    //   this.$socketManager.stompClient.connected
-    // ) {
-    this.setReady();
-    // } else {
-    //   this.$socketManager.connect(stompClient => {
-    //     this.setReady();
-    //   });
-    // }
+    if (
+      this.$socketManager.stompClient &&
+      this.$socketManager.stompClient.connected
+    ) {
+      this.setReady();
+    } else {
+      this.$socketManager.connect(stompClient => {
+        this.setReady();
+        this.requestSudoku();
+      });
+    }
   },
   methods: {
     updateGuess(payload) {
@@ -46,9 +66,20 @@ export default {
         {}
       );
     },
-    updatePotentialGuess(payload) {
+    addPotentialTile(payload) {
       this.$socketManager.stompClient.send(
         "/app/game/sudoku/addPotentialTile",
+        JSON.stringify({
+          x: payload.x,
+          y: payload.y,
+          guess: payload.newValue
+        }),
+        {}
+      );
+    },
+    removePotentialTile(payload) {
+      this.$socketManager.stompClient.send(
+        "/app/game/sudoku/removePotentialTile",
         JSON.stringify({
           x: payload.x,
           y: payload.y,
@@ -62,29 +93,10 @@ export default {
         "/user/game/sudoku/start",
         tick => {
           let response = JSON.parse(tick.body);
+          console.log(tick.body);
           if (response.ready) {
-            this.$api
-              .get("/game/sudoku", { params: { gameId: this.gameId } })
-              .then(response => {
-                const data = response.data;
-
-                var grid = [];
-                data.forEach((rowData, rowIndex) => {
-                  grid.push([]);
-
-                  rowData.forEach((tileData, colIndex) => {
-                    grid[tileData.xpos][tileData.ypos] = {
-                      solution: tileData.solution,
-                      guess: tileData.guess,
-                      potentialSolutions: tileData.potentialSolutions,
-                      xpos: tileData.xpos,
-                      ypos: tileData.ypos
-                    };
-                  });
-                });
-
-                this.grid = grid;
-              });
+            this.requestSudoku();
+            this.players = response.players;
           }
         }
       );
@@ -94,7 +106,31 @@ export default {
           {},
           JSON.stringify({})
         );
-      }, 500);
+      }, 1000);
+    },
+    requestSudoku() {
+      this.$api
+        .get("/game/sudoku", { params: { gameId: this.gameId } })
+        .then(response => {
+          const data = response.data;
+
+          var grid = [];
+          data.forEach((rowData, rowIndex) => {
+            grid.push([]);
+
+            rowData.forEach((tileData, colIndex) => {
+              grid[tileData.xpos][tileData.ypos] = {
+                solution: tileData.solution,
+                guess: tileData.guess,
+                potentialSolutions: tileData.potentialSolutions,
+                xpos: tileData.xpos,
+                ypos: tileData.ypos
+              };
+            });
+          });
+
+          this.grid = grid;
+        });
     }
   }
 };
